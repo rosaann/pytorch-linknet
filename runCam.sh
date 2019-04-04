a9b911807ac886e55e67f33b6ddea5fd879e0ae6
rosaann <147429240@qq.com>
2019-04-04 14:40:09 +0800

    d

---
+++ runCam.sh
@@ -0,0 +1,4 @@
+#bash
+python main.py --datapath /media/0016/ --cachepath /dataCache/cam/ --dataset cs --model models/model.py --save /Trained_models/cityscapes/ --saveTrainConf True --saveAll True --plot True
+
+
