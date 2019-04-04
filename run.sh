16e3b73f140b96e614332e905f80ccfd2702cdca
rosaann <147429240@qq.com>
2019-04-04 14:46:57 +0800

    d

--- run.sh
+++ run.sh
@@ -1,9 +1,2 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr  4 14:22:17 2019
-
-@author: zl
-"""
-
+#!/bin/bash
 python main.py --datapath /data/gtCoarse/gtCoarse/ --cachepath /dataCache/coarse/ --dataset cs --model models/model.py --save /Trained_models/cityscapes/ --saveTrainConf True --saveAll True --plot True
