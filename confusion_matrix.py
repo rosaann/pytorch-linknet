4d7571b8cc193cbd3e99b99c4e3184dc0aec1bc4
rosaann <147429240@qq.com>
2019-04-17 14:03:08 +0800

    d

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -18,8 +18,8 @@ class ConfusionMatrix:
         self.matStartIdx = 1 if not self.useUnlabeled else 0
 
     def update_matrix(self, target, prediction):
-        print('target ', target)
-        print('prediction ', prediction)
+        print('target ', target.shape)
+        print('prediction ', prediction.shape)
         if not(isinstance(prediction, np.ndarray)) or not(isinstance(target, np.ndarray)):
             print("Expecting ndarray")
         elif len(target.shape) == 3:          # batched spatial target
@@ -52,10 +52,10 @@ class ConfusionMatrix:
         else:
             print("Data with this dimension cannot be handled")
 
-        print('temp_target ', temp_target)
-        print('temp_prediction ', temp_prediction)
+        print('temp_target ', temp_target.shape, ' ', temp_target)
+        print('temp_prediction ', temp_prediction.shape, ' ', temp_prediction)
         self.mat += confusion_matrix(temp_target, temp_prediction, labels=self.list_classes)
-        print('self.mat ', self.mat)
+        print('self.mat ', self.mat.shape, ' ', self.mat)
 
     def scores(self):
         tp = 0
